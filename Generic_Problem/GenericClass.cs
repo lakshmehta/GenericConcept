afe0500bbf0f2dbdce7584c0d23220d3f44f1167
unknown <lakshmehta06@gmail.com>
2021-04-18 21:27:33 +0530

    [Laksh added NUMBER of PARAMETER]

--- Generic_Problem/GenericClass.cs
+++ Generic_Problem/GenericClass.cs
@@ -8,35 +8,20 @@ namespace Generic_Problem
 {
     public class GenericClass<T> where T : IComparable
     {
-      
-       public static int maximum(int x, int y, int z)
+        public static T maximum(params T[] elements)
         {
-            int max = x;
-            if (y.CompareTo(max) > 0)
-                max = y;
-            if (z.CompareTo(max) > 0)
-                max = z;
+            T max = elements[0];
+            foreach (T element in elements)
+            {
+                if (element.CompareTo(max) > 0)
+                {
+                    max = element;
+                }
+            }
             return max;
         }
-        public static float maximum(float x, float y, float z)
-        {
-            float max = x;
-            if (y.CompareTo(max) > 0)
-                max = y;
-            if (z.CompareTo(max) > 0)
-                max = z;
-            return max;
-        }
-        public static string maximum(string x, string y, string z)
-        {
-            string max = x;
-            if (y.CompareTo(max) > 0)
-                max = y;
-            if (z.CompareTo(max) > 0)
-                max = z;
-            return max;
-        }
+
+
 
     }
-
 }
